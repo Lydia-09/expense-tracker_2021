87bf292c6753a626c1b041ee7598788ff93fd74f
Lydia-09 <pasky0901@gmail.com>
2021-10-30 15:06:52 +0800

    feat: add update expense function

--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -14,6 +14,28 @@ router.get('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Expense.findById(id)
+    .lean()
+    .then((expense) => res.render('edit', { expense }))
+    .catch(error => console.log(error))
+})
+
+router.post('/:id/edit', (req, res) => {
+  const id = req.params.id
+  const { name, date, amount} = req.body
+  return Expense.findById(id)
+    .then(expense => {
+      expense.name = name
+      expense.date = date
+      expense.amount = amount
+      return expense.save()
+    })
+    .then(()=> res.redirect(`/expenses/${id}`))
+    .catch(error => console.log(error))
+})
+
 router.post('/', (req, res) => {
   const { name, date, amount} = req.body
   return Expense.create({  name, date, amount })
