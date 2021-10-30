c97f425693029f631aa1e79e33ebbda33e17b64f
Lydia-09 <pasky0901@gmail.com>
2021-10-30 14:52:23 +0800

    feat: add detail page

--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -6,6 +6,14 @@ router.get('/new', (req, res) => {
   return res.render('new')
 })
 
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Expense.findById(id)
+    .lean()
+    .then((expense) => res.render('detail', { expense }))
+    .catch(error => console.log(error))
+})
+
 router.post('/', (req, res) => {
   const { name, date, amount} = req.body
   return Expense.create({  name, date, amount })
